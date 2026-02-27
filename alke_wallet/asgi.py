ecff699c19600e8133714996a7b784bc21db35f2
Leandro <leandro.marav@gmail.com>
2026-02-27 15:16:57 -0300

    refactor: Renombrar proyecto de alkeSolutions a alke_wallet y eliminar archivos innecesarios

--- alke_wallet/asgi.py
+++ alke_wallet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for alkeSolutions project.
+ASGI config for alke_wallet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alkeSolutions.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alke_wallet.settings')
 
 application = get_asgi_application()
